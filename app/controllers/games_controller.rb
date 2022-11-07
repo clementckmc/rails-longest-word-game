050aa0c013a0d523a6f1f1c45f4f6c054d74b928
Clement Choi <clementchoi1997@gmail.com>
2022-11-07 15:23:43 +0900

    implemented

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,50 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    # get grid size
+    grid_size = rand(5..10)
+    @letters = grid_size.times.map { ('A'..'Z').to_a[rand(0...26)] }
+  end
+
+  def score
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:attempt]}"
+    word_serialized = URI.open(url).read
+    word = JSON.parse(word_serialized)
+    if !word["found"]
+      @message = "Sorry but #{params[:attempt].upcase} does not seem to be a valid English word"
+    elsif !check_grid(params[:grid].chars, word["word"].chars)
+      @message = "Sorry but #{params[:attempt].upcase} can't be built out of #{params[:grid].split(",")}"
+    else
+      @message = "Well Done!"
+      @score = 30 - (Time.now - Time.parse(params[:start_time])) + params[:attempt].size # add time
+    end
+  end
+
+  private
+
+  def check_grid(grid, word)
+    in_the_grid = word.all? { |letter| grid.include?(letter.upcase) }
+    no_overuse = false
+    word.each do |letter|
+      if grid.include?(letter.upcase)
+        grid_index = grid.find_index { |char| char == letter.upcase }
+        grid.delete_at(grid_index) unless grid_index.nil?
+        letter.gsub!(letter, "")
+      end
+    end
+    no_overuse = true if word.join == ""
+    in_the_grid && no_overuse
+  end
+
+  def create
+    if user = User.authenticate(params[:username], params[:password])
+      # Save the user ID in the session so it can be used in
+      # subsequent requests
+      session[:current_user_id] = user.id
+      redirect_to root_url
+    end
+  end
+
+end
